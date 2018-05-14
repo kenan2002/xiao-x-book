2e169fe28f0fb5cd08f357193f3d341f4345fb40
Yue Wang <wangyue2002@gmail.com>
2018-05-14 16:52:18 +0800

    init

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const client = new RTMClient({
 });
 
 client.on(RTMClientEvents.ONLINE, function() {
-  console.log('RTM online');
+  start();
 });
 
 client.on(RTMClientEvents.OFFLINE, function() {
@@ -23,9 +23,52 @@ client.on(RTMClientEvents.OFFLINE, function() {
 });
 
 client.on(RTMClientEvents.EVENT, function(message) {
-  console.log('event message received: ', message);
+  // console.log('event message received: ', message);
 });
 
-client.send({
-  // your message body
-});
+async function listen(type, condition) {
+  return new Promise(resolve => {
+    client.on(RTMClientEvents.EVENT, handler);
+
+    function conditionMet(message) {
+      return !condition || condition(message);
+    }
+
+    function handler(message) {
+      if (message.type === type && conditionMet(message)) {
+        resolve(message);
+        client.removeListener(RTMClientEvents.EVENT, handler);
+      }
+    }
+  });
+}
+
+async function start() {
+  let {data} = await listen('channel_visible');
+  await sendMessage(client, {
+    vchannel_id: data.vchannel_id,
+    text: '现在有一个小 X 书的出版发行事业需要你来拯救！\n1. 不，我没有这个打算\n2. 好的，包在我身上',
+  });
+  let {text} = await listen('channel_message', message => message.text === '1' || message.text === '2');
+  console.log(text);
+  if (text === '1') {
+    await sendMessage(client, {
+      vchannel_id: data.vchannel_id,
+      text: 'Game over!',
+    });
+  } else {
+    await sendMessage(client, {
+      vchannel_id: data.vchannel_id,
+      text: '还没有实现',
+    });
+  }
+}
+
+function sendMessage(rtm, { vchannel_id, refer_key, text }) {
+  return rtm.send({
+    type: 'message',
+    text,
+    vchannel_id: vchannel_id,
+    refer_key
+  });
+}
