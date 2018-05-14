202f9a0ee368a2e6cdd2bc5526f554488aae3391
Haiyang Peng <phycgp@live.com>
2018-05-14 17:22:02 +0800

    refactor and add scripts

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const client = new RTMClient({
 });
 
 client.on(RTMClientEvents.ONLINE, function() {
+  console.log('RTM online');
   start();
 });
 
@@ -45,23 +46,49 @@ async function listen(type, condition) {
 
 async function start() {
   let {data} = await listen('channel_visible');
-  await sendMessage(client, {
-    vchannel_id: data.vchannel_id,
-    text: '现在有一个小 X 书的出版发行事业需要你来拯救！\n1. 不，我没有这个打算\n2. 好的，包在我身上',
-  });
-  let {text} = await listen('channel_message', message => message.text === '1' || message.text === '2');
-  console.log(text);
-  if (text === '1') {
+
+  async function reply(text) {
     await sendMessage(client, {
       vchannel_id: data.vchannel_id,
-      text: 'Game over!',
-    });
-  } else {
-    await sendMessage(client, {
-      vchannel_id: data.vchannel_id,
-      text: '还没有实现',
+      text
     });
   }
+
+  async function getChoice() {
+    let {text} = await listen('channel_message', message => message.text === '1' || message.text === '2' || message.text === '3');
+    return text;
+  }
+
+  async function a0() {
+    await reply('现在有一个小 X 书的出版发行事业需要你来拯救！\n1. 不，我没有这个打算\n2. 好的，包在我身上');
+    const text = await getChoice();
+
+    if (text === '1') {
+      reply('Game over!');
+      await a0();
+    } else {
+      await a1();
+    }
+  }
+
+  async function a3() {
+  }
+
+  async function a4() {
+  }
+
+  async function a1() {
+    reply('1. 我是一个独来独往的小 X 书创建者\n2. 我需要一个团队来共同完成这项承载人类精神灵魂的伟大事业');
+    const text = await getChoice();
+
+    if (text === '1') {
+      await a3();
+    } else {
+      await a4();
+    }
+  }
+
+  await a0();
 }
 
 function sendMessage(rtm, { vchannel_id, refer_key, text }) {
